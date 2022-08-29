d6fcfbc95d7324f9ffed1fe152111ce7c953abba
Walid Wazini <wazini25@gmail.com>
2022-08-29 15:11:05 +0800

    profile API n page pt.1

---
+++ pages/profile/[id].tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { GoVerified } from "react-icons/go";
+import axios from "axios";
+
+import VideoCard from "../../components/VideoCard";
+import NoResult from "../../components/NoResult";
+import { IUser, Video } from "../../types";
+import { BASE_URL } from "../../utils";
+
+interface IProps {
+  data: {
+    user: IUser;
+    userVideos: Video[];
+    userLikedVideos: Video[];
+  };
+}
+
+const Profile = ({ data }: IProps) => {
+  return (
+    <div>
+      <h2>Profile</h2>
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/profile/${id}`);
+
+  return {
+    props: { data: res.data },
+  };
+};
+
+export default Profile;
